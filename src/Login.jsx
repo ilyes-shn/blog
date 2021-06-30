704d054c6fec0298e4127e745b86cfe8ccce663a
ilyes-shn <infinityilyes@gmail.com>
2021-06-30 11:04:39 +0100

    fixed

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div style={{margin: '5%', padding: '10%'}}>
+        <div style={{margin: '5% auto', width: '300px'}}>
+          <h1>Login</h1>
             <form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar"/>
-  </div>
+ 
 
   <div >
-    <label for="uname" style={{margin: '0px 10px'}}><b>Username</b></label>
+    <label for="uname" style={{margin: '10px'}}><b>Username</b></label>
+    <br />
     <input type="text" placeholder="Enter Username" name="uname" required/>
-
-    <label for="psw" style={{margin: '0px 10px'}}><b>Password</b></label>
+<br />
+    <label for="psw" style={{margin: '10px'}}><b>Password</b></label>
+   <br />
     <input type="password" placeholder="Enter Password" name="psw" required/>
 
-    <button type="submit" style={{backgroundColor: 'tomato'}}>Login</button>
+    <button type="submit" style={{backgroundColor: 'tomato', width: '100%'}}>Login</button>
     <label style={{margin: '0px 10px'}}>
     Remember me 
     </label>
-      <input type="checkbox"  name="remember"/> 
-    <button type="button" class="cancelbtn" style={{backgroundColor: 'tomato'}}>Cancel</button>
+      <input type="checkbox"  name="remember" style={{height: '20px'}}/> 
+    <button type="button" class="cancelbtn" style={{backgroundColor: 'tomato', width: '100%'}}>Cancel</button>
+    <br />
     <span class="psw">Forgot <a href="/">password?</a></span>
   </div>
 
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 
 const Register = () => {
-    return (
-        <div style={{margin: '5%', padding: '10%'}}>
-            <form action="action_page.php" method="post">
+  return (
+      <div style={{margin: '5% auto', width: '300px'}}>
+        <h1>Register</h1>
+          <form action="action_page.php" method="post">
 
-  <div >
-    <label for="uname" style={{margin: '0px 10px'}}><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required/>
 
-    <label for="psw" style={{margin: '0px 10px'}}><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required/>
+<div >
+  <label for="uname" style={{margin: '10px'}}><b>Username</b></label>
+  <br />
+  <input type="text" placeholder="Enter Username" name="uname" required/>
+<br />
+  <label for="psw" style={{margin: '10px'}}><b>Password</b></label>
+ <br />
+  <input type="password" placeholder="Enter Password" name="psw" required/>
 
-    <button type="submit" style={{backgroundColor: 'tomato'}}>Register</button>
-    
-  </div>
+  <button type="submit" style={{backgroundColor: 'tomato', width: '100%'}}>Register</button>
+  
+</div>
 
 </form>
-        </div>
-    )
+      </div>
+  )
 }
+
 
 export default Register
